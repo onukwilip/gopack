7d2749694246b6b3355e8c8007bc76a73b14b0a6
onukwilip <onukwilip@gmail.com>
2023-04-12 08:14:39 +0100

    🔨🛠 Modified the babel.config.js file

--- gopack-template/babel.config.js
+++ gopack-template/babel.config.js
@@ -1,7 +1,29 @@
 const gopackConfig = require("./gopack.config");
 
-const reactRefresh =
-  process.env.MODE === "production" ? [] : ["react-refresh/babel"];
+// FUNCTION TO CHECK IF A LIBRARY EXISTS
+const validateLibrary = (string) => gopackConfig?.libraries?.includes(string);
+
+const supportedLibraries = {
+  // JAVASCRIPT AND REACT COMPATIBILTY
+  react: {
+    preset: ["@babel/preset-react", { runtime: "automatic" }],
+    plugins: process.env.MODE === "production" ? null : "react-refresh/babel",
+  },
+};
+
+// GET A LIST OF PROPERTIES OF ALL SUPPORTED LIBRARIES
+const getSupportedLibrariesProperties = (key) => {
+  if (!gopackConfig?.libraries) return [];
+
+  const properties = [];
+  for (const library of gopackConfig?.libraries) {
+    if (!supportedLibraries[library]) continue;
+    if (!supportedLibraries[library][key]) continue;
+    properties.push(supportedLibraries[library][key]);
+  }
+
+  return properties;
+};
 
 module.exports = {
   presets: [
@@ -11,7 +33,7 @@ module.exports = {
           { useBuiltIns: "usage", corejs: "3.8", debug: false },
         ]
       : "@babel/preset-env",
-    ["@babel/preset-react", { runtime: "automatic" }],
+    ...getSupportedLibrariesProperties("preset"),
   ],
-  plugins: [...reactRefresh],
+  plugins: [...getSupportedLibrariesProperties("plugins")],
 };
